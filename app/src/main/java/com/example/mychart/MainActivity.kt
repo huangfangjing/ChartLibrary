8a9f5638b1682952b54f2046b79306ca9e4bdf35
huangfangjing <3429812342@qq.com>
2024-04-10 16:42:46 +0800

    chart v1.0.0

---
+++ app/src/main/java/com/example/mychart/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mychart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.chartlibraby.R
+import com.example.chartlibraby.bean.StaticData
+import com.example.chartlibraby.bean.TwoLevelData
+import com.example.chartlibraby.widget.AiLineChartView
+import com.example.chartlibraby.widget.AiVerticalBarView
+import com.example.chartlibraby.widget.LineChartView
+import com.example.chartlibraby.widget.VerticalBarView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var lineView: AiLineChartView
+    private lateinit var barView: AiVerticalBarView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lineView = findViewById(R.id.lineView)
+
+        var lineData: List<TwoLevelData> =
+            Gson().fromJson(StaticData.DATA, object : TypeToken<ArrayList<TwoLevelData>>() {}.type)
+        lineView.buildDate(lineData, LineChartView.OnLineChartListener {
+            //点击事件，不需要则传null
+            Toast.makeText(baseContext, it.indexName, Toast.LENGTH_LONG).show()
+        })
+        lineView.setUseAnimation(true)//默认使用动画
+
+
+        barView = findViewById(R.id.barView)
+        var barData: List<TwoLevelData> = Gson().fromJson(
+            StaticData.DATA_BAR,
+            object : TypeToken<ArrayList<TwoLevelData>>() {}.type
+        )
+        barView.buildDate(barData, VerticalBarView.OnVerticalBarClick { position, levelData ->
+            //点击事件，不需要则传null
+            Toast.makeText(baseContext, levelData.indexName, Toast.LENGTH_LONG).show()
+        }, "人数", "分数")
+        barView.hideIndicate()
+        barView.setUseAnimation(true)//默认使用动画
+
+    }
+}
